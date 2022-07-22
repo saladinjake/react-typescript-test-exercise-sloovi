b7dfa345db693efb452d7ca841a17be3c99b61b5
saladinjake <juwavictor@gmail.com>
2022-07-22 17:51:14 +0100

    deployment debugging: trying to fix api proxy service

---
+++ server.js
@@ -0,0 +1,14 @@
+/*Load compiled build files on local server */
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(`${__dirname}/build/`));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './build/index.html'));
+});
+const PORT = process.env.PORT || 8000;
+const { log } = console;
+app.listen(PORT, () => {
+  log('Server started on port: ', PORT);
+
+});
