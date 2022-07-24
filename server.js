1a860f48d43ca7150326d1d46af938853daa04bf
saladinjake <juwavictor@gmail.com>
2022-07-25 00:17:32 +0100

    use method to deploy

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ var fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const proxyConfig = {
+  target: "https://stage.api.sloovi.com",
+  prependPath: true,
+  pathRewrite: {
+    '^/api': '/',
+  },
+};
+
 const cors = require("cors");
 
 app.use(express.static(`${__dirname}/build/`));
 app.use(cors())
+ // All routes starting with /api have this proxy middleware applied
+app.use(createProxyMiddleware('/api', proxyConfig));
 
 
 app.get('*', (req, res) => {
