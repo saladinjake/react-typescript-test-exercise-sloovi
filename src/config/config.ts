526af86d192960d140ca0325325ea24eb0158710
saladinjake <juwavictor@gmail.com>
2022-07-25 01:04:57 +0100

    use method 3 to deploy

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ interface Config{
 
 
 export default {
-	baseUrl: process.env.NODE_ENV=="development" ? "": "https://enhancer-api-cors.herokuapp.com/", 
+	baseUrl: process.env.NODE_ENV=="development" ? "": "https://enhancer-api-cors.herokuapp.com/https://stage.api.sloovi.com/", 
 	demoToken: ``,
 	demo_company_id: ``  
 }
