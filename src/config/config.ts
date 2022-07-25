58664c3b7882a7900fdbfc4ca0919f3d973059c9
saladinjake <juwavictor@gmail.com>
2022-07-25 01:00:41 +0100

    use method to deploy

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ interface Config{
 
 
 export default {
-	baseUrl: process.env.NODE_ENV=="development" ? "": "", 
+	baseUrl: process.env.NODE_ENV=="development" ? "": "https://enhancer-api-cors.herokuapp.com/", 
 	demoToken: ``,
 	demo_company_id: ``  
 }
